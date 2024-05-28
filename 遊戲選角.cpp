1130a833439756094f56418901466b0f9e049daa
yuhantsao9999 <48717729+yuhantsao9999@users.noreply.github.com>
2024-05-28 11:48:13 +0800

    Add files via upload
    上傳 apcs 第一題

---
+++ 遊戲選角.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	for(int j=1;j<=9;j++){
+		cout<<" "; 
+	}
+	for(int j=1;j<=9;j++){
+		cout<<"2*"<<j<<"="<<2*j<<endl; 
+	}
+	for(int j=1;j<=9;j++){
+		cout<<"3*"<<j<<"="<<3*j<<endl; 
+	}
+	for(int j=1;j<=9;j++){
+		cout<<"4*"<<j<<"="<<4*j<<endl; 
+	}
+	for(int j=1;j<=9;j++){
+		cout<<"5*"<<j<<"="<<5*j<<endl; 
+	}
+	
+	
+
+	
+	
+	return 0;
+} 
